ec08f9479aa34d13658c1ccd4880918fdb465f86
dilandcv <josemiguelvargas_@hotmail.com>
2026-03-27 10:13:58 -0500

    finalizado

--- app.py
+++ app.py
@@ -1,13 +1,15 @@
 from services import *
 from files import *
+#The code should be executed here in app.py
 
-inventory = []
-running = True
+inventory = []   # Main list of products
+running = True   # Control variable for the loop
 
 print("========")
 print("WELCOME")
 print("========")
 
+# Main loop
 while running:
     print("""
 1 - Add product
@@ -22,28 +24,35 @@ while running:
     """)
 
     try:
+        # Ask user for option
         option = int(input("Choose an option: "))
 
-        # ADD
+        # ADD PRODUCT
         if option == 1:
             name = input("Insert product name: ")
             price = float(input("Insert price: "))
             quantity = int(input("Insert quantity: "))
             add_product(inventory, name, price, quantity)
 
-        # SHOW
+        # SHOW INVENTORY
         elif option == 2:
             show_inventory(inventory)
 
-        # SEARCH
+        # SEARCH PRODUCT
         elif option == 3:
             name = input("Insert name: ")
             product = search_product(inventory, name)
-            print(product if product else "Product not found")
 
-        # UPDATE
+            if product:
+                print(product)
+            else:
+                print("Product not found")
+
+        # UPDATE PRODUCT
         elif option == 4:
             name = input("Insert product name: ")
+
+            # Allow skipping values
             price = input("New price (enter to skip): ")
             quantity = input("New quantity (enter to skip): ")
 
@@ -54,12 +63,12 @@ while running:
                 int(quantity) if quantity else None
             )
 
-        # DELETE
+        # DELETE PRODUCT
         elif option == 5:
             name = input("Product name: ")
             delete_product(inventory, name)
 
-        # STATS
+        # SHOW STATISTICS
         elif option == 6:
             stats = calculate_statistics(inventory)
 
@@ -82,10 +91,12 @@ while running:
             if new_data:
                 decision = input("Overwrite inventory? (Y/N): ").upper()
 
+                # Replace inventory
                 if decision == "Y":
                     inventory = new_data
                     print("Inventory replaced")
 
+                # Merge inventory
                 else:
                     for new in new_data:
                         existing = search_product(inventory, new["name"])
@@ -98,15 +109,14 @@ while running:
 
                     print("Inventory merged")
 
-        # EXIT
+        # EXIT PROGRAM
         elif option == 9:
             print("THANKS FOR USING")
-            running = False
+            running = False  # End loop
 
         else:
             print("Invalid option")
 
     except:
         print("Invalid input, try again")
-        
                                   
